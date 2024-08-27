27189e345a1349e0bcd8a2d2e92343e1c7febfe1
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2024-08-27 09:57:00 -0300

    27/08

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider";
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+    title: "Caboclo 7 Ondas",
+    description: "Criado por HFerraz",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="pt-br" suppressHydrationWarning>
+            <body
+                className={cn(
+                    "min-h-screen bg-background font-sans antialiased",
+                    fontSans.variable
+                )}
+            >
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+
+            </body>
+        </html>
+    );
+}
