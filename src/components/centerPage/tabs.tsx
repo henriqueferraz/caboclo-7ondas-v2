6f64b8d8e04f9301309f5b8d8cb9d0b4d6260003
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2024-09-02 16:18:41 -0300

    02/09

--- src/components/centerPage/tabs.tsx
+++ src/components/centerPage/tabs.tsx
@@ -1,23 +1,27 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getAllProducts } from "@/services/product";
 
-export function CenterTabs() {
+export const CenterTabs = async () => {
+
+    const products = await getAllProducts()
+
     return (
         <div>
             <Tabs defaultValue="tab01">
                 <TabsList className="flex">
                     <TabsTrigger
-                        className="flex-1flex-1 mt-6"
+                        className="flex-1"
                         value="tab01">
                         Tab 01
                     </TabsTrigger>
                     <TabsTrigger
-                        className="flex-1 mt-6"
+                        className="flex-1"
                         value="tab02">
                         Tab 02
                     </TabsTrigger>
                 </TabsList>
-                <TabsContent value="tab01">Conteúdo da Tab 01</TabsContent>
-                <TabsContent value="tab02">Conteúdo da Tab 02</TabsContent>
+                <TabsContent value="tab01" className="mt-6">Conteúdo da Tab 01</TabsContent>
+                <TabsContent value="tab02" className="mt-6">Conteúdo da Tab 02</TabsContent>
             </Tabs>
         </div>
     )
---
+++ src/services/product.ts
@@ -0,0 +1,5 @@
+import { Product } from "@/types/product";
+
+export const getAllProducts = async (): Promise<Product[]> => {
+    return [];
+}
